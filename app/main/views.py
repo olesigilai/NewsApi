1b79b3ff9f0f58daf78cf48e6dc06cd5641236ba
olesigilai <ole_sigilai@yahoo.com>
2021-02-20 22:40:01 +0300

    index and articles function defined

--- app/main/views.py
+++ app/main/views.py
@@ -8,48 +8,48 @@ Review=Review
 
 
 
-# # Views
-# @main.route('/')
-# def index():
+# Views
+@main.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-#     # Getting news sources
-#     popularity = get_news('popularity')
-#     bitcoin = get_news('bitcoin')
-#     business = get_news('business')
-#     techcrunch = get_news('techcrunch')
-#     wall_street = get_news('wsj')
+    # Getting news sources
+    popularity = get_news('popularity')
+    bitcoin = get_news('bitcoin')
+    business = get_news('business')
+    techcrunch = get_news('techcrunch')
+    wall_street = get_news('wsj')
 
-#     title = 'Home - Welcome to The best News Review Website Online'
+    title = 'Home - Welcome to The best News Review Website Online'
 
-#     search_news = request.args.get('news_query')
+    search_news = request.args.get('news_query')
 
-#     if search_news:
-#         return redirect(url_for('search', category_name = search_news))
-#     else:
-#         return render_template('index.html', title = title, popularity = popularity, bitcoin = bitcoin, business = business, techcrunch = techcrunch, wall_street = wall_street )
+    if search_news:
+        return redirect(url_for('search', category_name = search_news))
+    else:
+        return render_template('index.html', title = title, popularity = popularity, bitcoin = bitcoin, business = business, techcrunch = techcrunch, wall_street = wall_street )
 
 
-# @main.route('/new/articles/')
-# def articles():
+@main.route('/new/articles/')
+def articles():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-#     # Getting news sources
-#     focus = get_news('focus')
-#     techcrunch = get_news('techcrunch')
-#     india = get_news('the-times-of-india')
+    # Getting news sources
+    focus = get_news('focus')
+    techcrunch = get_news('techcrunch')
+    india = get_news('the-times-of-india')
 
-#     title = 'Home - Welcome to The best News Review Website Online'
+    title = 'Home - Welcome to The best News Review Website Online'
 
-#     search_news = request.args.get('news_query')
+    search_news = request.args.get('news_query')
 
-#     if search_news:
-#         return redirect(url_for('search', category_name = search_news))
-#     else:
-#         return render_template('articles.html', title = title, focus = focus, techcrunch = techcrunch, india = india )
+    if search_news:
+        return redirect(url_for('search', category_name = search_news))
+    else:
+        return render_template('articles.html', title = title, focus = focus, techcrunch = techcrunch, india = india )
