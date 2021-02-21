ddbcc2570399d58f7b7dbd91dca33e33f770c555
olesigilai <ole_sigilai@yahoo.com>
2021-02-21 10:49:13 +0300

    class News is completed

--- app/models.py
+++ app/models.py
@@ -11,32 +11,32 @@ class News:
         self.article = article
 
 
-# class Review:
+class Review:
 
-#     all_reviews = []
+    all_reviews = []
 
-#     def __init__(self,news_id,title,imageurl,review):
-#         self.news_id = news_id
-#         self.title = title
-#         self.imageurl = imageurl
-#         self.review = review
-
-
-#     def save_review(self):
-#         Review.all_reviews.append(self)
+    def __init__(self,news_id,title,imageurl,review):
+        self.news_id = news_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
 
 
-#     @classmethod
-#     def clear_reviews(cls):
-#         Review.all_reviews.clear()
+    def save_review(self):
+        Review.all_reviews.append(self)
 
-#     @classmethod
-#     def get_reviews(cls,id):
 
-#         response = []
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
 
-#         for review in cls.all_reviews:
-#             if review.news_id == id:
-#                 response.append(review)
+    @classmethod
+    def get_reviews(cls,id):
 
-#         return response
+        response = []
+
+        for review in cls.all_reviews:
+            if review.news_id == id:
+                response.append(review)
+
+        return response
